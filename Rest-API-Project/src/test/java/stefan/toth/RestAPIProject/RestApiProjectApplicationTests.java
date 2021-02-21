318d5644b0dd94290ee41c664d58ee02960552b7
Stefan <stefantoth3848@gmail.com>
2021-02-21 16:46:31 +0200

    Initial commit

---
+++ Rest-API-Project/src/test/java/stefan/toth/RestAPIProject/RestApiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package stefan.toth.RestAPIProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
