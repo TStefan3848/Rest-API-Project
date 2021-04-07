b37d41da012d7c10ecc05eb307ca67e519c3498d
Stefan <stefantoth3848@gmail.com>
2021-04-07 15:14:05 +0300

    Change - Changed method to get used memory per request

--- Rest-API-Project/src/main/java/stefan/toth/RestAPIProject/config/RequestStatisticsInterceptor.java
+++ Rest-API-Project/src/main/java/stefan/toth/RestAPIProject/config/RequestStatisticsInterceptor.java
@@ -9,31 +9,31 @@ import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.lang.management.ManagementFactory;
-import java.lang.management.MemoryMXBean;
 
 @Component
 public class RequestStatisticsInterceptor extends EmptyInterceptor implements HandlerInterceptor {
 
     private final Logger log = LoggerFactory.getLogger(RequestStatisticsInterceptor.class);
     private final String START_TIME = "Start_Time";
-
-    private final MemoryMXBean memoryMXBean = ManagementFactory.getMemoryMXBean();
-
+    private final String INITIAL_USED_MEMORY = "Initial_memory";
 
     @Override
     public boolean preHandle(HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull Object handler) {
         long startTime = System.currentTimeMillis();
-        request.setAttribute(START_TIME, startTime);
-        memoryMXBean.gc();
 
+        Runtime.getRuntime().gc();
+
+        request.setAttribute(START_TIME, startTime);
+        request.setAttribute(INITIAL_USED_MEMORY, Runtime.getRuntime().maxMemory() - Runtime.getRuntime().freeMemory());
         return true;
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull Object handler, Exception ex) {
         long executionTime = System.currentTimeMillis() - (long) request.getAttribute(START_TIME);
-        log.info("[" + handler + "] [Time: " + executionTime + "ms], [Heap-Memory usage: " + memoryMXBean.getHeapMemoryUsage().getUsed() + "], [Non-Heap-Memory usage: " + memoryMXBean.getNonHeapMemoryUsage().getUsed() + "]");
+
+        long currentMemory = Runtime.getRuntime().maxMemory() - Runtime.getRuntime().freeMemory();
+        log.info("[" + handler + "] [Time: " + executionTime + "ms], [Memory-Used: " + (currentMemory - (long) request.getAttribute(INITIAL_USED_MEMORY)) + " bytes]");
     }
 
 }
